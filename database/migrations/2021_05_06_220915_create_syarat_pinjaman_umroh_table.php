6533d6aa588c3e2e1eee2ca315e3a5abf4bb26b1
oong26 <mkhalil26122000@gmail.com>
2021-05-07 21:09:48 +0700

    api kelengkapan data

---
+++ database/migrations/2021_05_06_220915_create_syarat_pinjaman_umroh_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSyaratPinjamanUmrohTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('syarat_pinjaman_umroh', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_nasabah')->unsigned();
+            $table->string('suket_travel')->nullable();
+            $table->string('selfie_usaha')->nullable();
+            $table->string('siup')->nullable();
+            $table->string('nib')->nullable();
+            $table->string('scan_jaminan')->nullable();
+            $table->timestamps();
+
+            $table->foreign('id_nasabah')->references('id')->on('nasabah');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('syarat_pinjaman_umroh');
+    }
+}
