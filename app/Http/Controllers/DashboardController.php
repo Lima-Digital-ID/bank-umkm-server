61e7214dbf7b97250940bec6e9adb83ed0ff2837
Inant <inant.kharisma@gmail.com>
2021-05-29 09:53:28 +0700

    dashboard chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Models\Nasabah;
 use \App\Models\Pinjaman;
+use \App\Models\Pelunasan;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
@@ -18,17 +19,50 @@ class DashboardController extends Controller
             $this->param['nasabahVerified'] = Nasabah::where('is_verified', 1)->count();
             $this->param['pengajuanPinjaman'] = Pinjaman::where('status', 'Pending')->count();
             $this->param['pinjamanBerjalan'] = Pinjaman::where('status', 'Terima')->where('status_pencairan', 'Terima')->count();
-            // chart pinjaman harian bulan ini
-            $month = date('m');
-            $getRekapBulanIni = Pinjaman::select(DB::raw('SUM(nominal) as nominal'), 'tanggal_diterima')->groupBy('tanggal_diterima')->where(DB::raw('MONTH(tanggal_diterima)'), $month)->whereIn('status', ['Terima', 'Lunas'])->where('status_pencairan', 'Terima')->get();
+            // chart pinjaman bulanan
+            $bulan = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+            $tahun = date('Y');
+            $pinjamanBulanan = [];
+            $pembayaranBulanan = [];
+            $valuasi = [];
 
-            $tanggal = [];
-            $pinjamanHarian = [];
-            
-            foreach ($getRekapBulanIni as $key => $value) {
-                array_push($tanggal, date('d-m-Y', strtotime($value->tanggal_diterima)));
-                array_push($pinjamanHarian, $value->nominal);
+            foreach ($bulan as $key => $val) {
+                $pinjaman = Pinjaman::select(DB::raw('SUM(nominal) as nominal'))->where(DB::raw('MONTH(tanggal_diterima)'), $val)->where(DB::raw('YEAR(tanggal_diterima)'), $tahun)->whereIn('status', ['Terima', 'Lunas'])->where('status_pencairan', 'Terima')->get();
+                
+                $pembayaran = Pelunasan::select(DB::raw('SUM(nominal_pembayaran) as nominal_pembayaran'))->where(DB::raw('MONTH(tanggal_pembayaran)'), $val)->where(DB::raw('YEAR(tanggal_pembayaran)'), $tahun)->get();
+
+                if ($pembayaran) {
+                    array_push($pembayaranBulanan, $pembayaran[0]->nominal_pembayaran);
+                }
+                else{
+                    array_push($pembayaranBulanan, 0);
+                }
+                
+                if ($pinjaman) {
+                    array_push($pinjamanBulanan, $pinjaman[0]->nominal);
+                }
+                else{
+                    array_push($pinjamanBulanan, 0);
+                }
+
+                if ($pinjaman[0]->nominal || $pembayaran[0]->nominal_pembayaran) {
+                    array_push($valuasi, $pembayaran[0]->nominal_pembayaran - $pinjaman[0]->nominal);
+                }
+                else{
+                    array_push($valuasi, null);
+                }
+
             }
+
+            // $getRekapBulanan = Pinjaman::select(DB::raw('SUM(nominal) as nominal'), 'tanggal_diterima')->groupBy('tanggal_diterima')->where(DB::raw('YEAR(tanggal_diterima)'), $year)->whereIn('status', ['Terima', 'Lunas'])->where('status_pencairan', 'Terima')->groupBy(DB::raw('MONTH(tanggal_diterima)'))->get();
+
+            // $tanggal = [];
+            // $pinjamanHarian = [];
+            
+            // foreach ($getRekapBulanan as $key => $value) {
+            //     array_push($tanggal, date('d-m-Y', strtotime($value->tanggal_diterima))); //label grafik
+            //     array_push($pinjamanHarian, $value->nominal); //data grafik
+            // }
 
         } catch (\Exception $e) {
             return back()->withError('Terjadi kesalahan : '. $e->getMessage());
@@ -37,8 +71,11 @@ class DashboardController extends Controller
             return back()->withError('Terjadi kesalahan pada database : '. $e->getMessage());
         }
         return view('dashboard', $this->param)
-                ->with('tanggal',json_encode($tanggal))
-                ->with('nominalPinjamanHarian',json_encode($pinjamanHarian));
+                // ->with('all',json_encode($tanggal))
+                ->with('nominalPinjamanBulanan',json_encode($pinjamanBulanan))
+                ->with('nominalPembayaranBulanan',json_encode($pembayaranBulanan))
+                ->with('valuasi',json_encode($valuasi))
+                ->with('tahun',json_encode($tahun));
     }
     public function cekNotif()
     {
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -89,21 +89,113 @@
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 <script>
-    var tanggal = <?php echo $tanggal; ?>;
-    var nominalPinjamanHarian = <?php echo $nominalPinjamanHarian; ?>;
+    
+    var nominalPinjamanBulanan = <?php echo $nominalPinjamanBulanan; ?>;
+    var nominalPembayaranBulanan = <?php echo $nominalPembayaranBulanan; ?>;
+    var valuasi = <?php echo $valuasi; ?>;
+    var tahun = <?php echo $tahun; ?>;
+    console.log(nominalPinjamanBulanan)
+    console.log(nominalPembayaranBulanan)
+    console.log(valuasi)
+    // var nominalPembayaranHarian = 2000;
     var chartBulanIni = {
-        labels: tanggal,
-        datasets: [{
-            label: 'Nominal Pinjaman Harian',
-            backgroundColor: "transparent",
-            data: nominalPinjamanHarian,
-            borderColor: '#3c4099',
-            pointBackgroundColor: '#3c4099',
-            // tension: 1,
-            bezierCurve: true
-        }]
-    };
+        // labels: tanggal,
+        // datasets: [
+        //     {
+        //         label: 'Nominal Pinjaman Harian',
+        //         backgroundColor: "transparent",
+        //         data: nominalPinjamanHarian,
+        //         borderColor: '#3c4099',
+        //         pointBackgroundColor: '#3c4099',
+        //         // tension: 1,
+        //         bezierCurve: true
+        //     },
+        //     {
+        //         label: 'Nominal Pembayaran Harian',
+        //         backgroundColor: "transparent",
+        //         data: nominalPinjamanHarian,
+        //         borderColor: '##2ed8b6',
+        //         pointBackgroundColor: '##2ed8b6',
+        //         // tension: 1,
+        //         bezierCurve: true
+        //     }
+        // ]
+        labels: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
+  datasets: [{
+      label: "Pinjaman Bulanan",
+      fill: false,
+    //   lineTension: 0.1,
+      backgroundColor: "#3c4099",
+      borderColor: "#3c4099", // The main line color
+      borderCapStyle: 'butt',
+      borderDash: [], // try [5, 15] for instance
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "#3c4099",
+      pointBackgroundColor: "white",
+      pointBorderWidth: 2,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "#3c4099",
+      pointHoverBorderColor: "#3c4099",
+      pointHoverBorderWidth: 3,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: true
+      data: nominalPinjamanBulanan,
+      spanGaps: true,
+      bezierCurve: true,
+    }, {
+      label: "Pembayaran",
+      fill: false,
+    //   lineTension: 0.1,
+      backgroundColor: "#2ed8b6",
+      borderColor: "#2ed8b6",
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "#2ed8b6",
+      pointBackgroundColor: "white",
+      pointBorderWidth: 2,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "#2ed8b6",
+      pointHoverBorderColor: "#2ed8b6",
+      pointHoverBorderWidth: 3,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: false
+      data: nominalPembayaranBulanan,
+      spanGaps: true,
+      bezierCurve: true,
+    }, {
+      label: "Valuasi",
+      fill: false,
+    //   lineTension: 0.1,
+      backgroundColor: "#4099ff",
+      borderColor: "#4099ff",
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "#4099ff",
+      pointBackgroundColor: "white",
+      pointBorderWidth: 2,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "#4099ff",
+      pointHoverBorderColor: "#4099ff",
+      pointHoverBorderWidth: 3,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: false
+      data: valuasi,
+      spanGaps: true,
+      bezierCurve: true,
+    }
 
+  ]
+// };
+    };
+    
     window.onload = function() {
 
         var chrt = document.getElementById("rekapBulanIni").getContext("2d");
@@ -122,7 +214,7 @@
                 title: {
                   display: true,
                   position: "top",
-                  text: 'Rekap Jumlah Pinjaman Bulan Ini',
+                  text: 'Rekapitulasi Transaksi Bulanan Tahun ' + tahun,
                   fontSize: 16,
                   fontColor: "#3c4099",
                   fontFamily: 'Poppins'
